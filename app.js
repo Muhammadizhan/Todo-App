663e722d3b516cbf5d56a1f3217757c9d82717d6
Muhammadizhan <izhanayaz4@gmail.com>
2024-03-21 16:43:27 +0500

    changes

---
+++ app.js
@@ -0,0 +1,40 @@
+// let tasks = [];
+
+// function addTask() {
+//   const taskInput = document.getElementById("taskInput");
+//   const taskText = taskInput.value.trim();
+//   if (taskText !== "") {
+//     tasks.push(taskText);
+//     renderTasks();
+//     taskInput.value = "";
+//   }
+// }
+
+// function renderTasks() {
+//   const taskList = document.getElementById("taskList");
+//   taskList.innerHTML = "";
+//   tasks.forEach((task, index) => {
+//     const listItem = document.createElement("li");
+//     listItem.innerHTML = `
+//       <span>${task}</span>
+//       <button onclick="editTask(${index})">Edit</button>
+//       <button onclick="deleteTask(${index})">Delete</button>
+//     `;
+//     taskList.appendChild(listItem);
+//   });
+// }
+
+// function deleteTask(index) {
+//   tasks.splice(index, 1);
+//   renderTasks();
+// }
+
+// function editTask(index) {
+//   const newTaskText = prompt("Edit task:", tasks[index]);
+//   if (newTaskText !== null) {
+//     tasks[index] = newTaskText.trim();
+//     renderTasks();
+//   }
+// }
+
+// renderTasks();
