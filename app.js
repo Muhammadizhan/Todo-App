35fdec11f29da0a0dfecfe322c3a288e675bfba4
Muhammadizhan <izhanayaz4@gmail.com>
2024-03-21 17:10:24 +0500

    do some changes

--- app.js
+++ app.js
@@ -1,40 +1,40 @@
-// let tasks = [];
+let tasks = [];
 
-// function addTask() {
-//   const taskInput = document.getElementById("taskInput");
-//   const taskText = taskInput.value.trim();
-//   if (taskText !== "") {
-//     tasks.push(taskText);
-//     renderTasks();
-//     taskInput.value = "";
-//   }
-// }
+function addTask() {
+  const taskInput = document.getElementById("taskInput");
+  const taskText = taskInput.value.trim();
+  if (taskText !== "") {
+    tasks.push(taskText);
+    renderTasks();
+    taskInput.value = "";
+  }
+}
 
-// function renderTasks() {
-//   const taskList = document.getElementById("taskList");
-//   taskList.innerHTML = "";
-//   tasks.forEach((task, index) => {
-//     const listItem = document.createElement("li");
-//     listItem.innerHTML = `
-//       <span>${task}</span>
-//       <button onclick="editTask(${index})">Edit</button>
-//       <button onclick="deleteTask(${index})">Delete</button>
-//     `;
-//     taskList.appendChild(listItem);
-//   });
-// }
+function renderTasks() {
+  const taskList = document.getElementById("taskList");
+  taskList.innerHTML = "";
+  tasks.forEach((task, index) => {
+    const listItem = document.createElement("li");
+    listItem.innerHTML = `
+      <span>${task}</span>
+      <button onclick="editTask(${index})">Edit</button>
+      <button onclick="deleteTask(${index})">Delete</button>
+    `;
+    taskList.appendChild(listItem);
+  });
+}
 
-// function deleteTask(index) {
-//   tasks.splice(index, 1);
-//   renderTasks();
-// }
+function deleteTask(index) {
+  tasks.splice(index, 1);
+  renderTasks();
+}
 
-// function editTask(index) {
-//   const newTaskText = prompt("Edit task:", tasks[index]);
-//   if (newTaskText !== null) {
-//     tasks[index] = newTaskText.trim();
-//     renderTasks();
-//   }
-// }
+function editTask(index) {
+  const newTaskText = prompt("Edit task:", tasks[index]);
+  if (newTaskText !== null) {
+    tasks[index] = newTaskText.trim();
+    renderTasks();
+  }
+}
 
-// renderTasks();
+renderTasks();
